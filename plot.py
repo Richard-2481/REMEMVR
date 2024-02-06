1ff1bc8eaeb1cc4130059c2bfc649838b2c3251a
Dick Biggles <73739679+Richard-2481@users.noreply.github.com>
2024-02-06 16:59:28 +1000

    modified:   __pycache__/plot.cpython-310.pyc 	modified:   data/plots.json 	modified:   plot.py 	new file:   plots/Anxiety vs Age scatter.png 	new file:   plots/Anxiety vs Stress scatter.png 	new file:   plots/Depression vs Age scatter.png 	new file:   plots/Depression vs Anxiety scatter.png 	new file:   plots/Depression vs Stress scatter.png 	new file:   plots/Stress vs Age scatter.png

--- plot.py
+++ plot.py
@@ -35,65 +35,84 @@ def histogram(df, figure):
     plt.savefig(path, bbox_inches='tight')
     plt.clf()
 
-def scatter(df, variable, line):
-    if len(variable) == 3:
-        if line == 0:
-            plt.scatter(df[variable[0]], df[variable[1]], c=df[variable[2]], cmap='turbo', alpha=0.5)
-        else:
-            sns.regplot(x=variable[0], y=variable[1], data=df, order=line, line_kws={'color': 'red'})
-        plt.colorbar(label='Sex (0 for Female, 1 for Male)')
-        plt.title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()} Coloured by {variable[2].capitalize()}')
-        path = f'plots/{variable[0]}_{variable[1]}_{variable[2]}_scatter.png'    
+def scatter(df, figure):
+        
+    if len(figure['xyz']) == 3:
+        chart = sns.scatterplot(data=df, x=figure['xyz'][0], y=figure['xyz'][1], hue=figure['xyz'][2])
+        sns.regplot(x=figure['xyz'][0], y=figure['xyz'][1], data=df, scatter_kws={'alpha':0.5}, line_kws={'color': 'red'}, scatter=False)
 
+        chart.legend(title=figure['labels'][2], labels=[figure['labels'][4], figure['labels'][3]])
     else:
-        if line == 0:
-            plt.scatter(df[variable[0]], df[variable[1]], alpha=0.5)
-        else:
-            sns.regplot(x=variable[0], y=variable[1], data=df, order=line, line_kws={'color': 'red'})
-        plt.title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
-        path = f'plots/{variable[0]}_{variable[1]}_scatter.png'    
+        chart = sns.scatterplot(data=df, x=figure['xyz'][0], y=figure['xyz'][1])
 
-    plt.xlabel(variable[0].capitalize())
-    plt.ylabel(variable[1].capitalize())
+    chart.set_title(figure['name'])
+    chart.set_xlabel(figure['labels'][0])
+    chart.set_ylabel(figure['labels'][1])
+
+    path = f"plots/{figure['name']} scatter.png"     
+    
     plt.savefig(path, bbox_inches='tight')
     plt.clf()
 
-def scatter2(df, variable, line, residuals):
-    if residuals == 0:
-        fig, ax = plt.subplots(figsize=(5, 5))
-    else:
-        fig, axs = plt.subplots(1, 2, figsize=(12, 5))  # Create two subplots side by side
-        ax = axs[0]
+# def scatter(df, variable, line):
+#     if len(variable) == 3:
+#         if line == 0:
+#             plt.scatter(df[variable[0]], df[variable[1]], c=df[variable[2]], cmap='turbo', alpha=0.5)
+#         else:
+#             sns.regplot(x=variable[0], y=variable[1], data=df, order=line, line_kws={'color': 'red'})
+#         plt.colorbar(label='Sex (0 for Female, 1 for Male)')
+#         plt.title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()} Coloured by {variable[2].capitalize()}')
+#         path = f'plots/{variable[0]}_{variable[1]}_{variable[2]}_scatter.png'    
 
-    if len(variable) == 3:
-        if line == 0:
-            ax.scatter(df[variable[0]], df[variable[1]], c=df[variable[2]], cmap='turbo', alpha=0.5)
-        else:
-            sns.regplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, line_kws={'color': 'red'})
-        if residuals == 0:
-            fig.colorbar(ax.collections[0], ax=ax, label='Sex (0 for Female, 1 for Male)')
-        path = f'plots/{variable[0]}_{variable[1]}_{variable[2]}_scatter.png'
-    else:
-        if line == 0:
-            ax.scatter(df[variable[0]], df[variable[1]], alpha=0.5)
-        else:
-            sns.regplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, line_kws={'color': 'red'})
-        path = f'plots/{variable[0]}_{variable[1]}_scatter.png'
+#     else:
+#         if line == 0:
+#             plt.scatter(df[variable[0]], df[variable[1]], alpha=0.5)
+#         else:
+#             sns.regplot(x=variable[0], y=variable[1], data=df, order=line, line_kws={'color': 'red'})
+#         plt.title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
+#         path = f'plots/{variable[0]}_{variable[1]}_scatter.png'    
 
-    ax.set_title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
-    ax.set_xlabel(variable[0].capitalize())
-    ax.set_ylabel(variable[1].capitalize())
+#     plt.xlabel(variable[0].capitalize())
+#     plt.ylabel(variable[1].capitalize())
+#     plt.savefig(path, bbox_inches='tight')
+#     plt.clf()
 
-    if residuals == 1:
-        ax = axs[1]
-        sns.residplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, scatter_kws={'alpha': 0.5})
-        ax.set_title(f'Residual Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
-        ax.set_xlabel(variable[0].capitalize())
-        ax.set_ylabel('Residuals')
+# def scatter2(df, variable, line, residuals):
+#     if residuals == 0:
+#         fig, ax = plt.subplots(figsize=(5, 5))
+#     else:
+#         fig, axs = plt.subplots(1, 2, figsize=(12, 5))  # Create two subplots side by side
+#         ax = axs[0]
 
-    plt.tight_layout()
-    plt.savefig(path, bbox_inches='tight')
-    plt.clf()
+#     if len(variable) == 3:
+#         if line == 0:
+#             ax.scatter(df[variable[0]], df[variable[1]], c=df[variable[2]], cmap='turbo', alpha=0.5)
+#         else:
+#             sns.regplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, line_kws={'color': 'red'})
+#         if residuals == 0:
+#             fig.colorbar(ax.collections[0], ax=ax, label='Sex (0 for Female, 1 for Male)')
+#         path = f'plots/{variable[0]}_{variable[1]}_{variable[2]}_scatter.png'
+#     else:
+#         if line == 0:
+#             ax.scatter(df[variable[0]], df[variable[1]], alpha=0.5)
+#         else:
+#             sns.regplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, line_kws={'color': 'red'})
+#         path = f'plots/{variable[0]}_{variable[1]}_scatter.png'
+
+#     ax.set_title(f'Scatter Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
+#     ax.set_xlabel(variable[0].capitalize())
+#     ax.set_ylabel(variable[1].capitalize())
+
+#     if residuals == 1:
+#         ax = axs[1]
+#         sns.residplot(x=variable[0], y=variable[1], data=df, order=line, ax=ax, scatter_kws={'alpha': 0.5})
+#         ax.set_title(f'Residual Plot of {variable[0].capitalize()} v {variable[1].capitalize()}')
+#         ax.set_xlabel(variable[0].capitalize())
+#         ax.set_ylabel('Residuals')
+
+#     plt.tight_layout()
+#     plt.savefig(path, bbox_inches='tight')
+#     plt.clf()
 
 def violin(df):
     for column in df.columns:
@@ -132,7 +151,7 @@ def plot(df, figure):
         histogram(df, figure)
 
     if figure['plot_type'] == 'scatter':
-        scatter2(df, figure)
+        scatter(df, figure)
 
     if figure['plot_type'] == 'violin':
         violin(df, figure)
