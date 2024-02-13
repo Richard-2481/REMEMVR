a78da23d068f99ff07cf81cdf8d7387b5dfd2641
Not-Jayce <uqjrusht@uq.edu.au>
2024-02-13 10:01:06 +1000

    Added descriptives and pretty printing for each cognitive test

--- cogtests/cogtest.py
+++ cogtests/cogtest.py
@@ -36,59 +36,97 @@ def __init__():
 
         print("Saved working DataFrame!")
 
-def pretty_print(name: str, scores: dict):
-    """Taking a score name and a dictionary of subscores in the form `{subscore1: [list,of,values], subscore2: [list,of,values],...}`
-    prints a nice string displaying some descriptive stats for each score."""
-    print_str = ""
+def pretty_print(name: str, descriptives: dict[dict]):
+    """Taking an overall score name and a dictionary containing descriptives for each subscore
+    in the form `{subscore1name: {descriptives}, subscore2name: {descriptives},...}`.\n
+    Prints a nice string displaying the descriptive stats for each subscore."""
+    print_str = f"{name}:\n"
 
-    print_str += f"{name}:\n"
-
-    for (scorename, score) in scores.items():
-        score_desc = st.describe(score, nan_policy="omit")
-        
-        score_min = round(st.tmin(score, nan_policy="omit"))
-        score_max = round(st.tmax(score, nan_policy="omit"))
-
-        score_mean = round(score_desc.mean, 2)
-        score_sd = round(score_desc.variance**0.5, 2)
-
-        print_str += f"  {scorename}:\n    Min: {score_min}, Max: {score_max}\n    Mean: {score_mean}, SD: {score_sd}\n"
+    for (scorename, score_desc) in descriptives.items():
+        min, max, mean, stdev = score_desc.values()
+        print_str += f"   {scorename}:\n      Min: {min}, Max: {max}\n\
+      Mean: {mean}, SD: {stdev}\n"
     
     print(print_str)
+
+
+def get_descriptives(scores: dict):
+    """Taking a dictionary of subscores in the form `{subscore1: [list,of,values], subscore2: [list,of,values],...}`
+    returns a dictionary with the same keys but each value is a nested dict of relevant descriptive variables."""
+    descriptives = {}
+    for (scorename, score) in scores.items():
+        descriptives[scorename] = {}
+        score_desc = st.describe(score, nan_policy="omit")
+        
+        descriptives[scorename]["min"] = round(score_desc.minmax[0].__float__(),2)
+        descriptives[scorename]["max"] = round(score_desc.minmax[1].__float__(),2)
+
+        descriptives[scorename]["mean"] = round(score_desc.mean.__float__(), 2)
+        descriptives[scorename]["stdev"] = round(score_desc.variance.__float__()**0.5, 2)
+
+    return descriptives
 
    
 def NART():
     """Compare normative data for the NART."""
     nart_score = working_df["NART_score"].values
     nart_time = working_df["NART_time"].values
+
+    desc = get_descriptives({"Score": nart_score, "Time": nart_time})
     
-    pretty_print("Nart", {"Score": nart_score, "Time": nart_time})
+    pretty_print("Nart", desc)
 
 
 def RAVLT():
     """Compare normative data for the RAVLT."""
-    subtests = ['trial_1','trial_2','trial_3','trial_4','trial_5','trial_distraction','trial_free_recall','trial_delayed_recall']
+    subtests = ['trial_1','trial_5','trial_distraction','trial_free_recall','trial_delayed_recall']
+    # Not included: 'trial_2','trial_3','trial_4'
     subtest_dict = {}
     for name in subtests:
-        subtest_dict[f"{name.replace('_', ' ')} Score"] = working_df[f"RAVLT_{name}_score"].values
-        subtest_dict[f"{name.replace('_', ' ')} Time"] = working_df[f"RAVLT_{name}_time"].values
+        subtest_dict[f"{name.replace('_', ' ').title()} Score"] = working_df[f"RAVLT_{name}_score"].values
+        # Not included: subtest_dict[f"{name.replace('_', ' ').title()} Time"] = working_df[f"RAVLT_{name}_time"].values
 
-    subtest_dict["Delay Duration"] = working_df[f"RAVLT_delayed_recall_duration"].values
+    subtest_dict["Delay Duration Time"] = working_df[f"RAVLT_delayed_recall_duration"].values
+
+    desc = get_descriptives(subtest_dict)
     
-    pretty_print("RAVLT", subtest_dict)
+    pretty_print("RAVLT", desc)
 
 
 def BVMT():
     """Compare normative data for the BVMT."""
-    pass
+    subtests = ['total_recall', 'learning', 'percent_recalled', 'recognition_hits',
+    'recognition_false_alarms', 'recognition_discrimination_index', 'recognition_response_bias']
+    subtest_dict = {}
+    for name in subtests:
+        subtest_dict[f"{name.replace('_', ' ').title()} Score"] = working_df[f"BVMT_{name}"].values
+    
+    desc = get_descriptives(subtest_dict)
+    
+    pretty_print("BVMT", desc)
 
 
 def RPM():
     """Compare normative data for the RPM."""
     rpm_score = working_df["RPM_score"].values
     rpm_time = working_df["RPM_time"].values
+
+    desc = get_descriptives({"Score": rpm_score, "Time": rpm_time})
     
-    pretty_print("RPM", {"Score": rpm_score, "Time": rpm_time})
+    pretty_print("RPM", desc)
+
+
+def Demographics():
+    """Get demographic descriptive data"""
+    subtests = ['age', 'sex', 'education', 'vr_experience', 'typical_sleep', 'depression', 'anxiety', 'stress']
+
+    subtest_dict = {}
+    for name in subtests:
+        subtest_dict[f"{name.replace('_', ' ').title()}"] = working_df[f"{name}"].values
+    
+    desc = get_descriptives(subtest_dict)
+    
+    pretty_print("Demographics", desc)
 
 # Run init on import
 __init__()
